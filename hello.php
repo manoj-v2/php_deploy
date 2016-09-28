162990aeabb98e77299211019c66381927058719
Manoj P M <manoj.pm@v2solutions.com>
2016-09-28 11:14:07 +0530

    Initial commit

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Deploy PHP</title>
+</head>
+
+<body>
+<?php
+  print "<h2>Deploy PHP App</h2>";
+  print "Hello world!<br>";
+?>
+</body>
+
+</html>
